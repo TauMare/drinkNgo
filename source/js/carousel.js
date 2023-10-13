e4803e7c33f2623f53b5fda1956788c8777e0366
TauMare <sasas.sasas.972@gmail.com>
2023-10-13 23:27:08 +0700

    Kinda preprod

--- source/js/carousel.js
+++ source/js/carousel.js
@@ -1,78 +1,56 @@
-const slider = document.querySelector('.slider')
+const sliderNode = document.querySelector('.slider')
 
-const imageNodes = document.querySelectorAll('.slider__image-container');
+const sliderNodes = document.querySelectorAll('.slider__slide-wrapper')
 const stepNodes = document.querySelectorAll('.slider__control');
-const controlBtns = document.querySelectorAll('.slider__button');
-
-const nameNode = document.querySelector('.slider__title');
-const subtextNode = document.querySelector('.slider__subtitle');
+const controlNodes = document.querySelectorAll('.slider__button');
 
 const state = {
   currentSlide: 0,
-  buttons: {
-    next: {
-      active: true,
-    },
-    previous: {
-      active: false,
-    }
-  },
-  slides: [ {name: 'Декаф Флэт Уайт',
-  text: 'Свежесваренный кофе без кофеина из Эфиопии <br>с натуральным фермерским молоком — то, что нужно<br> для расслабления после тяжёлого рабочего дня'},  {
-    name: 'Лавандовый Латте',
-    text: 'Невероятное сочетание перуанской высокогорной <br> арабики с молоком ламы и лавандовым сиропом унесёт<br> вас прямо на вершину Радужных гор',
-  }, {
-    name: 'Тройной Эспрессо',
-    text: 'Мощнее укола адреналина, чернее самой тёмной ночи,<br> этот тройной эспрессо из Колумбии покажет вам, что такое<br> настоящая бодрость',
-  }]
 }
 
 const render = () => {
-  imageNodes.forEach((element) => {
-    element.classList.remove('slider__image-container--active');
-    element.querySelector('.slider__image').classList.remove('slider__image--active');
+  console.log(state.currentSlide)
+  sliderNodes.forEach((element) => {
+    element.classList.remove('slider__slide-wrapper--active')
   })
 
   stepNodes.forEach((element) => {
     element.classList.remove('slider__control--active');
   })
 
-  slider.classList.remove('slider--lavender');
-  slider.classList.remove('slider--espresso');
+  sliderNode.classList.remove('slider--lavender');
+  sliderNode.classList.remove('slider--espresso');
 
-  imageNodes.forEach((element, iterator) => {
+  sliderNodes.forEach((element, iterator) => {
     if (iterator === state.currentSlide) {
-      element.classList.add('slider__image-container--active');
-      element.querySelector('.slider__image').classList.add('slider__image--active');
-      stepNodes[iterator].classList.add('slider__control--active');
-      nameNode.textContent = state.slides[iterator].name;
-      subtextNode.innerHTML = state.slides[iterator].text;
+      element.classList.add('slider__slide-wrapper--active')
+      stepNodes[iterator].classList.add('slider__control--active')
     }
   })
 
   if (state.currentSlide === 1) {
-    controlBtns[0].disabled = false;
-    controlBtns[1].disabled = false;
-    slider.classList.add('slider--lavender');
+    controlNodes[0].disabled = false;
+    controlNodes[1].disabled = false;
+    sliderNode.classList.add('slider--lavender');
   }
   if (state.currentSlide === 0) {
-    controlBtns[0].disabled = true;
-    controlBtns[1].disabled = false;
+    controlNodes[0].disabled = true;
+    controlNodes[1].disabled = false;
   }
   if (state.currentSlide === 2) {
-    controlBtns[1].disabled = true;
-    controlBtns[0].disabled = false;
-    slider.classList.add('slider--espresso');
+    controlNodes[1].disabled = true;
+    controlNodes[0].disabled = false;
+    sliderNode.classList.add('slider--espresso');
   }
 }
 
 const init = () => {
   render();
-  controlBtns[1].addEventListener('click', () => {
+  controlNodes[1].addEventListener('click', () => {
     state.currentSlide = state.currentSlide + 1;
     render();
   });
-  controlBtns[0].addEventListener('click', () => {
+  controlNodes[0].addEventListener('click', () => {
     state.currentSlide = state.currentSlide - 1
     render();
   })
